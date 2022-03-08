7816388f78de73766b0ae508a81bc74e4efe82bf
Mohit Jamle <95996704+MohitJamle@users.noreply.github.com>
2022-03-08 21:04:47 +0530

    this is update

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{position: "fixed"}}>
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           Golden View
